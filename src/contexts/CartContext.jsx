ddb24aad807587beba91661672c5e578e82e0bc4
Pranish Bajracharya <pranish2017bajracharya@gmail.com>
2023-12-10 19:56:56 +0545

    upatedCart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-const [cart, setCart ] = useState([]);
+  const [cart, setCart ] = useState([]);
 
   const addToCart = (id, product) => {
     const newItem = {...product, quantity: 1};
@@ -24,17 +24,52 @@ const [cart, setCart ] = useState([]);
         }  else {
             setCart([...cart, newItem] )
         }
-}
+  }
 
   const deleteCartItem = (id) => {
     setCart((prevItem) => prevItem.filter(item => item.id !== id ));
   }
+
+    const incrementCart = (id) => {
+      const cartItem = cart.find((item) => item.id === id);
+      if( cartItem) {
+        const updatedCart = [...cart].map((item) => {
+          if( item.id === id) {
+            return {...item, quantity : cartItem.quantity + 1}
+          } else{
+            return item;
+          }
+        })
+        setCart(updatedCart)
+      } else {
+        setCart(cartItem)
+      }
+    }
+
+    const decreaseCart = (id) => {
+      const cartItem = cart.find((item) => item.id === id );
+      if( cartItem) {
+        const updatedCart = [...cart].map((item) => {
+          if( item.id === id ) {
+            return {...item, quantity: cartItem.quantity === 0 ? 0 : cartItem.quantity - 1 }
+          } else {
+            return item;
+          }
+        })
+        setCart(updatedCart)
+      } else {
+        setCart(cartItem);
+      }
+    }
+ 
 
   return (
     <CartContext.Provider value={{ 
       addToCart,
       cart,
       deleteCartItem,
+      incrementCart,
+      decreaseCart,
     }}>
       { children }
     </CartContext.Provider>
